d78c4e5ecb668b38ef8732a4e8f3d063c056bcdb
Silas Maciel Ferreira <silas.ferreira@ifood.com.br>
2023-10-19 18:16:30 -0300

    Adding features

--- script.js
+++ script.js
@@ -2,19 +2,37 @@ let started;
 let timer;
 let interval;
 
+const gabaritos = [
+  "A chuva cai suavemente, tocando a terra com carinho, trazendo vida e renovação a cada gota que desce do céu.",
+  "Em dias chuvosos, a natureza canta uma melodia serena, convidando-nos a refletir e apreciar sua beleza.",
+  "Cada pingo de chuva conta uma história, unindo-se para criar rios, alimentar florestas e abraçar a terra.",
+];
+
 $(document).ready(function () {
   started = false;
+
+  definirGabarito();
 
   $("#test-area").on("input", start);
 
   $("#reset").on("click", reset);
+
+  $("#test-area").bind("paste", function (e) {
+    e.preventDefault();
+  });
 });
+
+function definirGabarito() {
+  $("#expected").text(gabaritos[Math.floor(Math.random() * gabaritos.length)]);
+}
 
 function reset() {
   started = false;
   clearInterval(interval);
   $("#test-area").val("");
   $("#timer").text("00:00:00");
+  $("#timer").css("color", "black");
+  definirGabarito();
 }
 
 function start() {
@@ -28,15 +46,19 @@ function start() {
     }, 1000);
   } else {
     if ($("#test-area").val() == $("#expected").text()) {
-        clearInterval(interval);
-        $("#timer").css("color", "green");
+      clearInterval(interval);
+      $("#timer").css("color", "green");
     }
   }
 }
 
 function formatTime(date) {
   return (
-    concat(date.getUTCHours()) + ":" + concat(date.getUTCMinutes()) + ":" + concat(date.getUTCSeconds())
+    concat(date.getUTCHours()) +
+    ":" +
+    concat(date.getUTCMinutes()) +
+    ":" +
+    concat(date.getUTCSeconds())
   );
 }
 
